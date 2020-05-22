1becf8f9b45cd71db0a9e2badf20986e8caf5225
aomine <aomine1745@gmail.com>
2020-05-21 19:10:44 -0500

    v1.0

---
+++ js/app.js
@@ -0,0 +1,7 @@
+const progress = document.getElementById('progressbar')
+const totalHeight = document.body.scrollHeight - window.innerHeight
+window.onscroll = function () {
+	const progressHeight = (window.pageYOffset / totalHeight) * 100
+	progress.style.height = progressHeight + '%'
+
+}
